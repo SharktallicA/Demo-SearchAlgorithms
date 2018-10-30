8fb5a8bb3a5f3714343d835cdd2900d5eb8b6367
SharktallicA <22660310+SharktallicA@users.noreply.github.com>
2018-10-30 16:57:30 +0000

    extra work on Dijsktra's

--- Dijsktra.h
+++ Dijsktra.h
@@ -5,9 +5,12 @@
 class Dijsktra : public Algorithm
 {
 private:
+	vector<int> distances;
+
+	vector<Node*> previous;
 
 	//Processes the distances between neighbouring Nodes to the current Node
-	void calculateTenatatives(void)
+	void calculateTentatives(void)
 	{
 		//Index of current Node within map vector
 		int index = distance(map.begin(), find(map.begin(), map.end(), current));
@@ -15,15 +18,35 @@ private:
 		//If visited, exit immediately
 		if (visited[index]) return;
 
-		int distance;
 		Node* neighbour;
 
 		for (NodeData nD : current->GetValidAdjacentsData())
 		{
+			//Index of neighbour Node within map vector
+			int nIndex = distance(map.begin(), find(map.begin(), map.end(), nD.node));
+
 			neighbour = nD.node;
 
+			//Tenatative distance
+			int tDist = distances[index] + nD.cost;
+
+			if (tDist < distances[nIndex])
+			{
+				distances[nIndex] = tDist;
+				previous[nIndex] = nD.node;
+			}
 		}
 		visited[index] = true;
+	}
+
+	void findNext(void)
+	{
+		int currentBestDist = 0;
+
+		for (int i = 0; i < current->GetValidAdjacentsData().size(); i++)
+		{
+			
+		}
 	}
 
 public:
@@ -33,10 +56,12 @@ public:
 	//Runs Dijsktra's shortest path finding algorithm and returns the path taken
 	string Run(void)
 	{
+		distances = vector<int>(map.size());
+		for (int d : distances) d = 0;
 
 		while (current != endRef)
 		{
-
+			calculateTentatives();
 		}
 
 		return "";
